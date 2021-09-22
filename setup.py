33b0c639677db3851d4d409d7666e2fe06d7ad2b
Richard Neumann <mail@richard-neumann.de>
2021-09-22 16:32:23 +0200

    Add main project files

---
+++ setup.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python3
+"""Install script."""
+
+from setuptools import setup
+
+
+setup(
+    name='tenantforum',
+    use_scm_version={
+        "local_scheme": "node-and-timestamp"
+    },
+    setup_requires=['setuptools_scm'],
+    install_requires=[
+        'configlib',
+        'mdb',
+        'peewee',
+        'peeweeplus',
+        'wsgilib'
+    ],
+    author='HOMEINFO - Digitale Informationssysteme GmbH',
+    author_email='<info@homeinfo.de>',
+    maintainer='Richard Neumann',
+    maintainer_email='<r.neumann@homeinfo.de>',
+    packages=['tenantforum'],
+    license='GPLv3',
+    description='Tenant forum library.'
+)
---
+++ tenantforum/__init__.py
@@ -0,0 +1,1 @@
+"""Renant forum library."""
